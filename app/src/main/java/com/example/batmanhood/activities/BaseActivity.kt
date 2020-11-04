1fe3a3c66d9a228a6e6247ff2ffad8f14faa1320
Christian H <christian.hernandez.2015@gmail.com>
2020-11-03 19:58:04 -0800

    Fixed some sign in errors and status bar colors.

--- app/src/main/java/com/example/batmanhood/activities/BaseActivity.kt
+++ app/src/main/java/com/example/batmanhood/activities/BaseActivity.kt
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.example.batmanhood.R
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.dialog_progress.*
 
 open class BaseActivity : AppCompatActivity() {
@@ -52,5 +54,18 @@ open class BaseActivity : AppCompatActivity() {
         ).show()
 
         Handler().postDelayed({ doubleBackToExitPressedOnce = false }, 2000)
+    }
+
+    fun showErrorSnackBar(message: String) {
+        val snackBar =
+                Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
+        val snackBarView = snackBar.view
+        snackBarView.setBackgroundColor(
+                ContextCompat.getColor(
+                        this@BaseActivity,
+                        R.color.snackbar_error_color
+                )
+        )
+        snackBar.show()
     }
 }
--- app/src/main/java/com/example/batmanhood/activities/SignInActivity.kt
+++ app/src/main/java/com/example/batmanhood/activities/SignInActivity.kt
@@ -1,8 +1,11 @@
 package com.example.batmanhood.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import com.example.batmanhood.R
+import com.example.batmanhood.models.User
 import kotlinx.android.synthetic.main.activity_introduction.*
 import kotlinx.android.synthetic.main.activity_sign_in.*
 
@@ -13,7 +16,7 @@ class SignInActivity : BaseActivity() {
 
         setupActionBar()
 
-        button_sign_in_intro.setOnClickListener {
+        btn_sign_in.setOnClickListener {
             signInRegisteredUser()
         }
     }
@@ -42,7 +45,7 @@ class SignInActivity : BaseActivity() {
         // Here we get the text from editText and trim the space
         val email: String = et_email.text.toString().trim { it <= ' ' }
         val password: String = et_password.text.toString().trim { it <= ' ' }
-
+/*
         if (validateForm(email, password)) {
             // Show the progress dialog.
             showProgressDialog(resources.getString(R.string.please_wait))
@@ -61,7 +64,7 @@ class SignInActivity : BaseActivity() {
                         ).show()
                     }
                 }
-        }
+        }*/
     }
 
     /**
